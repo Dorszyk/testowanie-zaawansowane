e6122dee7c0b0918f1c1ddf76d40c477645b6050
Piotr Dorszynski <piotr.dorszynski@gmail.com>
2022-02-27 15:52:13 +0100

    UserServiceTest

--- main/src/test/java/com/sda/testing/solution/mockito/user/UserServiceTest.java
+++ main/src/test/java/com/sda/testing/solution/mockito/user/UserServiceTest.java
@@ -6,8 +6,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
 import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
@@ -38,8 +41,29 @@ class UserServiceTest {
     void shouldThrowExceptionWhenUserNotExists() {
         Long notExistingId = 878L;
         when(userRepository.findById((notExistingId))).thenReturn(Optional.empty());
-        assertThrows(Exception.class,() -> userService.getUserById(notExistingId));
+        assertThrows(Exception.class, () -> userService.getUserById(notExistingId));
 
+    }
+
+    @Test
+    void shouldThrowExceptionWhenAddingNotValidUser() {
+        User user = new User(875L, "Tomasz", "Dorszyk");
+        when(userValidator.isUserValid((user))).thenReturn(false);
+        assertThrows(Exception.class, () -> userService.addUser(user));
+    }
+
+    @Test
+    void shouldAddUser() {
+        User user = new User("Dupa", "Blada");
+        User savedUser = new User(2324L, user.getFirstName(), user.getLastName());
+
+        when(userValidator.isUserValid(user)).thenReturn(true);
+        when(userRepository.addUser(user)).thenReturn(savedUser);
+
+        User addedUser = userService.addUser(user);
+
+        assertEquals(savedUser, addedUser);
+        assertNotSame(user, addedUser);
     }
 
 }
