e6c5184347b267bbdb732f4d095147fc022941ea
Piotr Dorszynski <piotr.dorszynski@gmail.com>
2022-02-27 11:23:09 +0100

    conversion

--- main/src/test/java/com/sda/testing/solution/conversion/MeasurementConverterTest.java
+++ main/src/test/java/com/sda/testing/solution/conversion/MeasurementConverterTest.java
@@ -1,4 +1,15 @@
+package com.sda.testing.solution.conversion;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MeasurementConverterTest {
-  
+    @ParameterizedTest
+    @EnumSource(value = ConversionType.class, names = {"YARDS_TO_METERS", "METERS_TO_YARDS"})
+    void test(ConversionType conversionType){
+
+    }
+
 }
